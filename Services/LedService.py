90eb12e63bc5d93e21ce76a06476da6c5f986562
Will <william.franklin@cityfibre.com>
2022-01-06 17:19:30 +0000

    more

--- Services/LedService.py
+++ Services/LedService.py
@@ -5,7 +5,6 @@ import time
 
 class LedService:
     def __init__(self):
-        # led_freq = 800000
         number_of_pixels = 300
         pi_pin = board.D18
         self.pixels = neopixel.NeoPixel(pi_pin, number_of_pixels)
