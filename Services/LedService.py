e202ade7cf84589a64fcb782c2393b7d7f2e8edd
Will <william.franklin@cityfibre.com>
2022-01-06 18:51:58 +0000

    debug optional

--- Services/LedService.py
+++ Services/LedService.py
@@ -4,17 +4,18 @@ import time
 
 
 class LedService:
-    def __init__(self):
+    def __init__(self, *debug: bool):
         number_of_pixels = 300
         pi_pin = board.D18
         self.pixels = neopixel.NeoPixel(pi_pin, number_of_pixels)
+        self.debug = debug
 
-    def lightSingleLed(self, led_number, colour: [int, int, int], *debug: bool):
+    def lightSingleLed(self, led_number, colour: [int, int, int]):
         r = colour[0]
         g = colour[1]
         b = colour[2]
         self.pixels[led_number] = (r, b, g)
-        if debug: print(f"light {led_number} lit -- colour ({r}{g}{b})")
+        if self.debug: print(f"light {led_number} lit -- colour ({r}{g}{b})")
     
     def turnOffSingleLed(self, led_number):
         self.pixels[led_number] = (0, 0, 0)
@@ -24,7 +25,7 @@ class LedService:
         g = colour[1]
         b = colour[2]
         self.pixels[led_number] = (r, b, g)
-        print(f"light {led_number} lit")
+        if self.debug: print(f"light {led_number} lit")
         time.sleep(blink_length)
         self.turnOffSingleLed(led_number)
 
